c4d53fd0f0732d5d3df3af7d986960cb1e75ef85
johnnywang <johnny29621189@kimo.com>
2022-05-12 10:14:38 +0800

    UPD: update decorate comment

--- src/methods/decorate.ts
+++ src/methods/decorate.ts
@@ -15,12 +15,6 @@ export function handleDescriptor(target, key, descriptor, [decorator, ...args]) 
     configurable,
     enumerable,
     get() {
-      // 因為這邊以 getter 作為觸發
-      // 故原方法只要套用了一層 decorator 後面就都會拿到 isGetter: true
-      // 當 isGetter: true 時我們認定其為一個 decorator 進行取值
-      // 若 isGetter: false 時表示前面還沒套用任何 decorator，直接用原方法
-      // 將得到的 fn 丟進當前的 decorator 中執行得到最終的 value 方法
-      // 將 value 方法以 defineProperty 覆蓋回實例屬性的 descriptor
       const fn = isGetter ? originalGet.call(this) : originalValue
       const context = { target, key, fn }
       const value = decorator.call(this, context, ...args)
@@ -31,7 +25,6 @@ export function handleDescriptor(target, key, descriptor, [decorator, ...args]) 
         writable,
       }
 
-      // 這裡每個註冊的 decorator 只會各 set 1次
       defineProperty(this, key, desc)
 
       return value
